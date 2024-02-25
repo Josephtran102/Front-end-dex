02b0e33f7050adbf47d4bdcf7c80f2de4c499a35
Joseph Tran <115780760+Josephtran102@users.noreply.github.com>
2024-02-25 12:43:48 +0700

    Update details.ts

--- const/details.ts
+++ const/details.ts
@@ -1,6 +1,6 @@
 import { Chain, ScrollSepoliaTestnet } from "@thirdweb-dev/chains";
 
 export const TOKEN_ADDRESS: string =
-  "0x9D8f0C1404ED8F0dFE4C36F59ee4282861a00e99";
-export const DEX_ADDRESS: string = "0xA15C4Baf527F720a04c176Ef4709b1aec173b9BB";
+  "0x2e5Ce107bE6852CF287194D3fbC14B4E9DF5051E";
+export const DEX_ADDRESS: string = "0xaF03D9d62fFdbe11177136a18559a00b616d03F9";
 export const ACTIVE_CHAIN: Chain = ScrollSepoliaTestnet;
